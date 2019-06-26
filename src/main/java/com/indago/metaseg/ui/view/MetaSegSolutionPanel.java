2adfdfda24d01a7b92cea9df9eca7e97709d70fc
mp007 <praka027@umn.edu>
2019-06-27 00:03:01 +0200

    A very naive attempt at displaying all conflict sets with mouse hover (definitely needs to change, more of a fun and stupid attempt, doesn't work either)

--- src/main/java/com/indago/metaseg/ui/view/MetaSegSolutionPanel.java
+++ src/main/java/com/indago/metaseg/ui/view/MetaSegSolutionPanel.java
@@ -5,9 +5,15 @@ package com.indago.metaseg.ui.view;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -15,13 +21,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
+import org.scijava.Context;
+
+import com.indago.fg.Assignment;
 import com.indago.metaseg.MetaSegLog;
 import com.indago.metaseg.ui.model.MetaSegSolverModel;
 import com.indago.metaseg.ui.util.SolutionExporter;
+import com.indago.pg.IndicatorNode;
+import com.indago.pg.segments.SegmentNode;
 import com.indago.ui.util.UniversalFileChooser;
 
 import bdv.util.Bdv;
 import bdv.util.BdvHandlePanel;
+import net.imagej.ops.OpMatchingService;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.roi.IterableRegion;
+import net.imglib2.roi.geom.real.Polygon2D;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -37,6 +55,7 @@ public class MetaSegSolutionPanel extends JPanel implements ActionListener {
 	private JButton btnContinueMetatrain;
 
 	private JButton btnExport;
+	OpService ops = new Context( OpService.class, OpMatchingService.class ).getService( OpService.class );
 
 	public MetaSegSolutionPanel( final MetaSegSolverModel solutionModel ) {
 		super( new BorderLayout() );
@@ -51,6 +70,39 @@ public class MetaSegSolutionPanel extends JPanel implements ActionListener {
 						.options()
 						.is2D() ) );
 		viewer.add( model.bdvGetHandlePanel().getViewerPanel(), BorderLayout.CENTER );
+		model.bdvGetHandlePanel().getViewerPanel().getDisplay().addMouseListener( new MouseAdapter() {
+
+			@Override
+			public void mouseMoved( MouseEvent e ) {
+				RealPoint p = new RealPoint( 3 );
+				model.bdvGetHandlePanel().getViewerPanel().getGlobalMouseCoordinates( p );
+				int time = model.bdvGetHandlePanel().getViewerPanel().getState().getCurrentTimepoint();
+				Assignment< IndicatorNode > solution = model.getPgSolution( time );
+				ArrayList< SegmentNode > chosenSegs = new ArrayList<>();
+				for ( final SegmentNode segVar : model.getProblems().get( time ).getSegments() ) {
+					if ( solution.getAssignment( segVar ) == 1 ) {
+						chosenSegs.add( segVar );
+					}
+				}
+				for ( SegmentNode segmentNode : chosenSegs ) {
+					IterableRegion< ? > region = segmentNode.getSegment().getRegion();
+					Polygon2D poly = ops.geom().contour( ( RandomAccessibleInterval ) region, true );
+					List< RealLocalizable > vertices = poly.vertices();
+					int[] xpoints = new int[ vertices.size() ];
+					int[] ypoints = new int[ vertices.size() ];
+					for ( int i = 0; i < vertices.size(); i++ ) {
+						xpoints[ i ] = ( int ) vertices.get( i ).getFloatPosition( 0 );
+						ypoints[ i ] = ( int ) vertices.get( i ).getFloatPosition( 1 );
+					}
+					Polygon jPoly = new Polygon( xpoints, ypoints, xpoints.length );
+					if ( jPoly.contains( new Point( ( int ) p.getDoublePosition( 0 ), ( int ) p.getDoublePosition( 1 ) ) ) ) {
+						System.out.println( "centroid:" + ops.geom().centroid( poly ) );
+					}
+				}
+
+			}
+
+		} );
 		model.populateBdv();
 
 		final MigLayout layout = new MigLayout( "", "[][grow]", "" );
